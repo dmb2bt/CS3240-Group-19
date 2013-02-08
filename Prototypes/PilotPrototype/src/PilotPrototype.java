bce96f618c2e71a05c5cb13a8af3774d9282b0cd
David Bertoglio <dmb2bt@virginia.edu>
2013-02-08 13:09:12 -0500

    Added basic movement prototype

---
+++ Prototypes/PilotPrototype/src/PilotPrototype.java
@@ -0,0 +1,17 @@
+import lejos.nxt.*;
+import lejos.robotics.navigation.*;
+
+public class PilotPrototype {
+	public static void main(String[] args){
+		System.out.println("Testing move");
+		DifferentialPilot driver = new DifferentialPilot(2.1f, 4.4f, Motor.B, Motor.C, true);
+		System.out.println("Moving forward");
+		driver.travel(5);
+		System.out.println("Moving backwards");
+		driver.travel(-5);
+		System.out.println("Turning Left");
+		driver.rotate(90);
+		System.out.println("Turning Right");
+		driver.rotate(-90);
+	}
+}
