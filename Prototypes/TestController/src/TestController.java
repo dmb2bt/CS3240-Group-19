1217e6e33db57f57b9ce4196afdeb5fd86b11481
David Bertoglio <dmb2bt@virginia.edu>
2013-03-02 17:32:22 -0500

    added to the test program

--- Prototypes/TestController/src/TestController.java
+++ Prototypes/TestController/src/TestController.java
@@ -1,4 +1,24 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Scanner;
+
+import lejos.pc.comm.NXTComm;
+import lejos.pc.comm.NXTCommException;
+import lejos.pc.comm.NXTCommFactory;
+import lejos.pc.comm.NXTInfo;
 
 public class TestController {
+    private NXTComm connection;
+    private NXTInfo[] info;
+    private long start, latency;
+    static Boolean readFlag = true;
+    static Object lock = new Object();
+
+    public static void main(String[] args) {
+
+    }
 
 }
