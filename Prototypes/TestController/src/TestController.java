6e72271c9068107411c4ca7c6331fb821a66d743
David Bertoglio <dmb2bt@virginia.edu>
2013-03-04 00:26:39 -0500

    Added most of the basic command creation

--- Prototypes/TestController/src/TestController.java
+++ Prototypes/TestController/src/TestController.java
@@ -22,10 +22,10 @@ public class TestController {
     private DataOutputStream oHandle;
     private DataInputStream iHandle;
 
-    public static void main(String[] args) throws NXTCommException{
+    public static void main(String[] args) throws NXTCommException {
         start = System.currentTimeMillis();
 
-        if(USBTest){
+        if (USBTest) {
             connection = NXTCommFactory.createNXTComm(NXTCommFactory.USB);
             info = connection.search(null, 0);
         } else {
@@ -57,7 +57,7 @@ public class TestController {
                         start = System.currentTimeMillis();
                         byte[] buffer = new byte[256];
                         int count = iHandle.read(buffer); // might wnt to check ack later
-                        if (count>0){
+                        if (count > 0) {
                             String ret = (new String(buffer)).trim();
                             long l = System.currentTimeMillis() - start;
                             System.out.printf("NXJ: %s [%dms]\n", ret, l);
@@ -67,7 +67,7 @@ public class TestController {
                         System.out.println("Fail to read from iHandle bc "
                                 + e.toString());
                         return;
-                    } catch (InterruptedException e){
+                    } catch (InterruptedException e) {
 
                     }
 
@@ -76,20 +76,26 @@ public class TestController {
         };
         PCreceiver.start();
 
-        System.out.println("\n PC: Waiting on command....");
+        System.out.println("\n PC: Enter Commands (help or ? for commands list)");
 
-        do{
+        do {
             try {
                 input = scanner.nextLine();
-                start = System.currentTimeMillis();
+                if (input.equalsIgnoreCase("help") || input.equals("?")) {
+                    System.out.println(getCommandHelp());
+                } else {
+                    start = System.currentTimeMillis();
+                    String message = createCommand(input);
+                    if (message.equals("")) {
+                        System.out.println("Maformed Input, try again.");
+                    } else {
+                        oHandle.write(message.getBytes());
+                        oHandle.flush();
+                        latency = System.currentTimeMillis() - start;
 
-                oHandle.write(createCommand(input).getBytes());
-                oHandle.flush();
-                latency = System.currentTimeMillis() - start;
-
-                System.out.println("\nPC: " + input + " [" + latency + "ms]");
-
-
+                        System.out.println("\nPC: " + input + " [" + latency + "ms]");
+                    }
+                }
             } catch (IOException e) {
                 System.out.println("Fail to write to oHandle bc "
                         + e.toString());
@@ -99,7 +105,7 @@ public class TestController {
 
         try {
             connection.close();
-            readFlag=false; // stop reading threads
+            readFlag = false; // stop reading threads
             // stop all threads as well
         } catch (IOException e) {
             // TODO Auto-generated catch block
@@ -108,8 +114,142 @@ public class TestController {
         System.out.println("Ending session...");
     }
 
-    public String createCommand(String cmd){
+    public String createCommand(String cmd) {
         // TO DO: Create commands based on the input...
+        String message = "";
+        String[] cmdWords = cmd.split(" ");
+        String command = getCommand(cmdWords);
+        String[] args = getCommandArguments(cmdWords);
+
+        if (cmdWords.length > 1) {
+            return message;
+        } else {
+            if (command.equalsIgnoreCase("move")) {
+                message = createMoveCommand(cmdWords);
+            } else if (command.equalsIgnoreCase("arc")) {
+                message = createArcCommand(cmdWords);
+            } else if (command.equalsIgnoreCase("turn")) {
+                message = createTurnCommand(cmdWords);
+            } else if (command.equalsIgnoreCase("stop")) {
+                message = createStopCommand();
+            } else if (command.equalsIgnoreCase("setspeed")) {
+                message = createSetSpeedMessage();
+            } else if (command.equalsIgnoreCase("read")) {
+                message = createReadSensorMessage();
+            } else if (command.equalsIgnoreCase("none")) {
+                message = createNoOpMessage();
+            }
+        }
+        return message;
+    }
+
+    public String[] getCommandArguments(String[] words) {
+        String[] args = new String[words.length - 1];
+        for (int i = 1; i < words.length; i++) {
+            args[i - 1] = words[i];
+        }
+        return args;
+    }
+
+    public String getCommand(String[] words) {
+        return words[0];
+    }
+
+    public boolean isNumeric(String number) {
+        try {
+            int i = Integer.parseDouble(number);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    public String createMoveCommand(String[] args) {
+        String command = "MS";
+        if (args.length < 1) {
+            return "";
+        } else {
+            if (args[0].equalsIgnoreCase("forward")) {
+                command += "F";
+            } else if (args[0].equalsIgnoreCase("backward")) {
+                command += "B";
+            }
+            if (args.length >= 2) {
+                if (isNumeric(args[1])) {
+                    command += args[1];
+                } else {
+                    return "";
+                }
+            } else {
+                command += "0000000";
+            }
+        }
+        return command;
+    }
+
+    public String createArcCommand(String[] args) {
+        String command = "MA";
+        if (args.length < 1) {
+            return "";
+        } else {
+            if (args[0].equalsIgnoreCase("forward")) {
+                command += "F";
+            } else if (args[0].equalsIgnoreCase("backward")) {
+                command += "B";
+            } else {
+                return "";
+            }
+            if (args.length >= 2){
+                if(args[1].equalsIgnoreCase("left")){
+                    command += "L";
+                } else if (args[1].equalsIgnoreCase("right")){
+                    command += "R";
+                } else {
+                    return "";
+                }
+            }
+            command += "090";
+            command += "000";
+        }
+        return command;
+    }
+
+    public String createTurnCommand(String[] args) {
+        String command = "TN";
+        if(args.length < 1){
+            return "";
+        } else {
+            if(args[0].equalsIgnoreCase("right")){
+                command += "R";
+            } else if (args[0].equalsIgnoreCase("left")){
+                command += "L";
+            } else {
+                return "";
+            }
+            if (args.length > 1){
+                if(2)
+            }
+        }
+    }
+
+    public String createStopCommand() {
+        return "ST00000000";
+    }
+
+    public String createSetSpeedMessage(String[] args) {
+
+    }
+
+    public String createReadSensorMessage(String[] args) {
+
+    }
+
+    public String createNoOpMessage() {
+        return "0000000000";
+    }
+
+    public String getCommandHelp() {
+        return "";
     }
 
 
