f45ddea93169828e3f01eebab9dfa5620062441b
Steven Hauser <sch5zc@virginia.edu>
2013-04-14 16:19:34 -0400

    debugger updates (reads sensors and such)

--- Prototypes/DebuggerShell/src/DebuggerShell.java
+++ Prototypes/DebuggerShell/src/DebuggerShell.java
@@ -31,6 +31,7 @@ import javax.swing.JScrollPane;
 
 import lejos.pc.comm.NXTCommFactory;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 
 public class DebuggerShell implements KeyListener, ActionListener {
@@ -45,6 +46,10 @@ public class DebuggerShell implements KeyListener, ActionListener {
 
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTextField txtLightUnread;
+	private JTextField txtSoundUnread;
+	private JTextField txtUltrasonicUnread;
+	private JTextField txtTouchUnread;
 	
 	public DebuggerShell(Debugger d){
 		this.debugger = d;
@@ -59,32 +64,32 @@ public class DebuggerShell implements KeyListener, ActionListener {
 		f.getContentPane().setLayout(null);
 		
 		JButton btnEstablishConnection = new JButton("Establish Connection");
-		btnEstablishConnection.setBounds(516, 13, 154, 25);
+		btnEstablishConnection.setBounds(516, 13, 191, 25);
 		f.getContentPane().add(btnEstablishConnection);
 		btnEstablishConnection.addActionListener(this);
 		
 		JButton btnEndConnection = new JButton("End Connection");
-		btnEndConnection.setBounds(516, 51, 154, 25);
+		btnEndConnection.setBounds(516, 51, 191, 25);
 		f.getContentPane().add(btnEndConnection);
 		btnEndConnection.addActionListener(this);
 		
 		JButton btnForward = new JButton("Forward");
-		btnForward.setBounds(516, 422, 154, 25);
+		btnForward.setBounds(516, 422, 191, 25);
 		f.getContentPane().add(btnForward);
 		btnForward.addActionListener(this);
 		
 		JButton btnLeft = new JButton("Left");
-		btnLeft.setBounds(516, 460, 69, 25);
+		btnLeft.setBounds(516, 460, 89, 25);
 		f.getContentPane().add(btnLeft);
 		btnLeft.addActionListener(this);
 		
 		JButton btnRight = new JButton("Right");
-		btnRight.setBounds(601, 460, 69, 25);
+		btnRight.setBounds(617, 460, 90, 25);
 		f.getContentPane().add(btnRight);
 		btnRight.addActionListener(this);
 		
 		JButton btnBackward = new JButton("Backward");
-		btnBackward.setBounds(516, 497, 154, 25);
+		btnBackward.setBounds(516, 497, 191, 25);
 		f.getContentPane().add(btnBackward);
 		btnBackward.addActionListener(this);
 		
@@ -109,15 +114,88 @@ public class DebuggerShell implements KeyListener, ActionListener {
 		textField_1.setBounds(12, 498, 24, 22);
 		f.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
+		
+		JLabel lblSensorsclickTo = new JLabel("Sensors");
+		lblSensorsclickTo.setBounds(516, 89, 154, 16);
+		f.getContentPane().add(lblSensorsclickTo);
+		
+		txtLightUnread = new JTextField();
+		txtLightUnread.setEditable(false);
+		txtLightUnread.setText("Light: unread");
+		txtLightUnread.setBounds(516, 118, 116, 22);
+		f.getContentPane().add(txtLightUnread);
+		txtLightUnread.setColumns(10);
+		
+		txtSoundUnread = new JTextField();
+		txtSoundUnread.setEditable(false);
+		txtSoundUnread.setText("Sound: unread");
+		txtSoundUnread.setColumns(10);
+		txtSoundUnread.setBounds(516, 153, 116, 22);
+		f.getContentPane().add(txtSoundUnread);
+		
+		txtUltrasonicUnread = new JTextField();
+		txtUltrasonicUnread.setEditable(false);
+		txtUltrasonicUnread.setText("Ultrasonic: unread");
+		txtUltrasonicUnread.setColumns(10);
+		txtUltrasonicUnread.setBounds(516, 188, 116, 22);
+		f.getContentPane().add(txtUltrasonicUnread);
+		
+		txtTouchUnread = new JTextField();
+		txtTouchUnread.setEditable(false);
+		txtTouchUnread.setText("Touch: unread");
+		txtTouchUnread.setColumns(10);
+		txtTouchUnread.setBounds(516, 223, 116, 22);
+		f.getContentPane().add(txtTouchUnread);
+		
+		JButton btnReadSound = new JButton("Read");
+		btnReadSound.setActionCommand("Read Sound");
+		btnReadSound.setBounds(638, 118, 69, 25);
+		btnReadSound.addActionListener(this);
+		f.getContentPane().add(btnReadSound);
+		
+		JButton btnReadLight = new JButton("Read");
+		btnReadLight.setActionCommand("Read Light");
+		btnReadLight.setBounds(638, 152, 69, 25);
+		btnReadLight.addActionListener(this);
+		f.getContentPane().add(btnReadLight);
+		
+		JButton btnReadUltrasonic = new JButton("Read");
+		btnReadUltrasonic.setActionCommand("Read Ultrasonic");
+		btnReadUltrasonic.addActionListener(this);
+		btnReadUltrasonic.setBounds(638, 187, 69, 25);
+		f.getContentPane().add(btnReadUltrasonic);
+		
+		JButton btnReadTouch = new JButton("Read");
+		btnReadTouch.setBounds(638, 222, 69, 25);
+		btnReadTouch.setActionCommand("Read Touch");
+		btnReadTouch.addActionListener(this);
+		f.getContentPane().add(btnReadTouch);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setVisible(true);
-		f.setSize(700, 580);
+		f.setSize(737, 584);
 		
 		printMessage("Debugger Started.\nPlease press \"ESTABLISH CONNECTION\" to begin debugging.");
 		
 
 		textField.requestFocus();
 		textField.requestFocusInWindow();
+	}
+	
+	public void set(String sensor, int value){
+		switch(sensor){
+		case "U":
+			txtUltrasonicUnread.setText("Ultrasonic: " + value);
+			break;
+		case "S":
+			txtSoundUnread.setText("Sound: " + value);
+			break;
+		case "L":
+			txtLightUnread.setText("Light: " + value);
+			break;
+		case "T":
+			txtTouchUnread.setText("Touch: " + value);
+			break;
+		}
 	}
 	
 	
@@ -188,6 +266,15 @@ public class DebuggerShell implements KeyListener, ActionListener {
 			debugger.establishConnection();
 		} else if(ac.equals("End Connection")){
 			debugger.endConnection();
-		} else System.out.println(ac);
+		} else if(ac.equals("Read Sound")){
+			debugger.sendMessage(debugger.createCommand("read m"));
+		} else if(ac.equals("Read Light")){
+			debugger.sendMessage(debugger.createCommand("read l"));
+		} else if(ac.equals("Read Ultrasonic")){
+			debugger.sendMessage(debugger.createCommand("read u"));
+		} else if(ac.equals("Read Touch")){
+			debugger.sendMessage(debugger.createCommand("read t"));
+		}
+		else System.out.println(ac);
 	}
 }
